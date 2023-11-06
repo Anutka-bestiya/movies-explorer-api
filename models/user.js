e77694867102763ff8af3ed2e337817ff6158dd4
Anna b <anchikfyz@yandex.ru>
2023-11-06 06:46:10 +0500

    ref

--- models/user.js
+++ models/user.js
@@ -1,48 +1,50 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs'); // импортируем bcrypt
-const UnauthorizedError = require('../errors/unauthorized-err');
-const {
-  BAD_AUTH_DATA,
-  BAD_EMAIL,
-} = require('../utils/message');
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs"); // импортируем bcrypt
+const UnauthorizedError = require("../errors/unauthorized-err");
+const { BAD_AUTH_DATA, BAD_EMAIL } = require("../utils/message");
 
-const userSchema = new mongoose.Schema({
-  email: {
-    type: String, // строка
-    required: true, // обязательное поле
-    validate: {
-      validator: (email) => /.+@.+\..+/.test(email),
-      message: BAD_EMAIL,
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String, // строка
+      required: true, // обязательное поле
+      validate: {
+        validator: (email) => /.+@.+\..+/.test(email),
+        message: BAD_EMAIL,
+      },
+      unique: true,
     },
-    unique: true,
+    password: {
+      type: String, // строка
+      required: true, // обязательное поле
+      select: false, // необходимо добавить поле select чтобы API не возвращал хеш пароля
+    },
+    name: {
+      type: String, // строка
+      minlength: [2, "Имя пользователя должно быть длиной от 2 символов"],
+      maxlength: [30, "Имя пользователя должно быть длиной до 30 символов"],
+      required: true, // обязательное поле
+    },
   },
-  password: {
-    type: String, // строка
-    required: true, // обязательное поле
-    select: false, // необходимо добавить поле select чтобы API не возвращал хеш пароля
-  },
-  name: {
-    type: String, // строка
-    minlength: [2, 'Имя пользователя должно быть длиной от 2 символов'],
-    maxlength: [30, 'Имя пользователя должно быть длиной до 30 символов'],
-    required: true, // обязательное поле
-  },
-}, { versionKey: false });
+  { versionKey: false }
+);
 
-userSchema.statics.findUserByCredentials = (email, password) => this.findOne({ email }).select('+password')
-  .then((user) => {
-    if (!user) {
-      return Promise.reject(new UnauthorizedError(BAD_AUTH_DATA));
-    }
+userSchema.statics.findUserByCredentials = function (email, password) {
+  return this.findOne({ email })
+    .select("+password")
+    .then((user) => {
+      if (!user) {
+        return Promise.reject(new UnauthorizedError(BAD_AUTH_DATA));
+      }
 
-    return bcrypt.compare(password, user.password)
-      .then((matched) => {
+      return bcrypt.compare(password, user.password).then((matched) => {
         if (!matched) {
           return Promise.reject(new UnauthorizedError(BAD_AUTH_DATA));
         }
 
         return user; // теперь user доступен
       });
-  });
+    });
+};
 
-module.exports = mongoose.model('user', userSchema);
+module.exports = mongoose.model("user", userSchema);
